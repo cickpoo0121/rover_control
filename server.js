38031266d7fa60e2b726ab3bec56bc94b7187370
Tee <cickpoo0121@gmail.com>
2022-05-14 23:00:28 +0700

    redme and test Pre-Commit Pre-Push

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.prepare().then(() => {
   server.use(bodyParser.urlencoded({ extended: false }));
   server.use(bodyParser.json());
 
+  // set up multer for upload file
   const multer = require("multer");
   const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -26,10 +27,7 @@ app.prepare().then(() => {
 
   const upload = multer({ storage: storage });
 
-  server.get("/hi", (req, res) => {
-    return res.json({ msg: "hi" });
-  });
-
+  // upload file and calculate result
   server.post("/uploadExcel", upload.single("files"), (req, res, next) => {
     const file = req.file;
 
@@ -49,7 +47,7 @@ app.prepare().then(() => {
       for (let i = 0; i < data.length; i++) {
         var obj = i === 0 ? {} : JSON.parse(JSON.stringify(result[i - 1]));
 
-        // set initail
+        // set the first line to N:0,0
         if (i === 0) {
           obj.input = data[0];
           obj.dir = "N";
@@ -59,45 +57,49 @@ app.prepare().then(() => {
           continue;
         }
 
-        // set input
+        // set input (R, L, F)
         obj.input = data[i];
 
         // set x,y when input F
         if (data[i] === "F") {
+          // E, W are affect to X axis
           obj.x =
-            obj.dir === "E"
-              ? obj.x + 1
-              : obj.dir === "W"
-              ? obj.x === 0
-                ? obj.x
-                : obj.x - 1
-              : obj.x;
+            obj.dir === "E" // if E is mean x increase 1 (move)
+              ? obj.x + 1 // x + 1
+              : obj.dir === "W" // if W is mean x decrease 1 (move)
+              ? obj.x === 0 // before decrease x check x is 0 or not
+                ? obj.x // if x equal 0, x = x is mean x = 0
+                : obj.x - 1 // if x not equal 0 then x decrease 1
+              : obj.x; // it means not E and W then not affect to y
 
+          // N, WS are affect to Y axis
           obj.y =
-            obj.dir === "N"
-              ? obj.y + 1
-              : obj.dir === "S"
-              ? obj.y === 0
-                ? obj.y
-                : obj.y - 1
-              : obj.y;
+            obj.dir === "N" // if N is mean y increase 1 (move)
+              ? obj.y + 1 // y + 1
+              : obj.dir === "S" // if S is mean x decrease 1 (move)
+              ? obj.y === 0 // before decrease y check y is 0 or not
+                ? obj.y // if y equal 0, y = y is mean y = 0
+                : obj.y - 1 // if y not equal 0 then y decrease 1
+              : obj.y; // it means not N and S then no affect to y
         } else {
           // set direction
-          if (direction.indexOf(obj.dir) === 0 && data[i] === "L") {
+          if (direction.indexOf(obj.dir) === 0 && data[i] === "L") { // when currenct direction is N and input is L then set direction to W
             obj.dir = "W";
           } else if (
             direction.indexOf(obj.dir) === direction.length - 1 &&
-            data[i] === "R"
-          ) {
+            data[i] === "R" 
+          ) { // when currenct direction is W and input is R then set direction to N
             obj.dir = "N";
           } else {
+            // set direction
             obj.dir =
               data[i] === "R"
                 ? direction[direction.indexOf(obj.dir) + 1]
                 : direction[direction.indexOf(obj.dir) - 1];
           }
         }
-
+        
+        // add result
         result.push(obj);
       }
       res.status(200).send({ msg: "Upload successfuly", data: result });
